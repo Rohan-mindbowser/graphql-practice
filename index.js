6f54fe1b7f13f9ccd9825fcac2dfa649fdff7ae1
Rohan Kadam <rohan.kadam@mindbowser.com>
2022-08-18 13:15:07 +0530

    wrote some basic queries and did mutation

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 const express = require("express");
 const { buildSchema } = require("graphql");
 const { graphqlHTTP } = require("express-graphql");
-
+const axios = require("axios");
 const app = express();
 
+var message = "hello there...";
+
 const schema = buildSchema(`
+
+    type Mutation{
+        setmessage(newMessage:String):String
+    }
+
+    type Post{
+        userId:Int,
+        id:Int,
+        title:String,
+        body:String
+    }
+
+    type User{
+        name:String,
+        age:Int,
+        college:String
+    }
+
     type Query{
         hello: String,
-        welcome:String
+        welcome(name: String):String,
+        getuser:User,
+        getuserlist:[User],
+        getpost:[Post]
     }
 `);
 
@@ -15,11 +38,44 @@ const root = {
   hello: () => {
     return "Hello world";
   },
-  welcome:()=>{
-    return "Welcome"
-  }
+  welcome: (args) => {
+    console.log(args);
+    return `Welcome ${args.name}`;
+  },
+  getuser: () => {
+    const user = {
+      name: "rohan",
+      age: 25,
+      college: "Modern college",
+    };
+    return user;
+  },
+  getuserlist: () => {
+    const users = [
+      {
+        name: "rohan",
+        age: 25,
+        college: "Modern college",
+      },
+      {
+        name: "sid",
+        age: 23,
+        college: "MIT college",
+      },
+    ];
+    return users;
+  },
+  getpost: async () => {
+    const posts = await axios.get("https://jsonplaceholder.typicode.com/posts");
+    return posts.data;
+  },
+  setmessage: ({ newMessage }) => {
+    message = newMessage;
+    return message;
+  },
 };
 
+//graphql endpoint
 app.use(
   "/graphql",
   graphqlHTTP({
